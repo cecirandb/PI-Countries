4145b5e4676212479b57f4bf8685f6846fce4cd5
cecirandb <cecicuellorandb@hotmail.com>
2021-12-05 18:26:54 -0300

    models done

---
+++ api/src/models/Activity.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('activity', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    difficulty: {
+        type: DataTypes.ENUM('1', '2', '3', '4', '5')
+    },
+    duration: {
+        type: DataTypes.STRING
+    },
+    season: {
+        type: DataTypes.ENUM('Summer', 'Autumn', 'Winter', 'Spring')
+    }
+  }, { timestamps: false });
+};
